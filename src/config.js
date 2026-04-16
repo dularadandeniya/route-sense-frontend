dbd29329d43aa93b5530d644dcf0320fc6430cbd
dularadandeniya <dulara.dandeniya@gmail.com>
2026-04-16 15:55:52 +0530

    fixing url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = "http://localhost:8080";
+// const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://route-sense-backend-production.up.railway.app";
 export default API_BASE_URL;
